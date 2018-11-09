8d92bddfd402a9dbcd639003b0eb3c6ae742483b
Julian Laubstein <contact@julianlaubstein.de>
2018-11-09 09:50:12 +0100

    Feature/semver (#41)
    * Added semver dependency
    
    * Added semver and ProgramBuilder
    
    * Updated Program documentation
    
    * Sudo
    
    * Added test to versions
    
    * Recompile tarpaulin every time

---
+++ src/consts.rs
@@ -0,0 +1,27 @@
+use semver::{Version, VersionReq};
+
+lazy_static! {
+    /// The current version of the melon crate
+    pub static ref VERSION: Version = env!("CARGO_PKG_VERSION").parse().unwrap();
+    pub static ref VERSION_REQ: VersionReq = {
+        let mut version_string: String = env!("CARGO_PKG_VERSION")
+            .split('.')
+            .take(2)
+            .collect::<Vec<_>>()
+            .join(".");
+
+        version_string.insert(0, '^');
+
+        VersionReq::parse(&version_string).expect("unable to parse version requirement")
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn requirement_matches() {
+        assert!(VERSION_REQ.matches(&VERSION));
+    }
+}
